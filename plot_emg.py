51e2592dd464656e77ccefa33a422720532cccb0
Alexandre Naaim <alexandre.naaim@gmail.com>
2018-04-19 13:44:08 +0200

    integration de la premiere version de plot_emg

---
+++ plot_emg.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 13:25:48 2018
+
+@author: Alexandre Naaim
+"""
+
+import numpy as np
+
+
+def plot_emg(filename):
+    reader = btk.btkAcquisitionFileReader()
+    reader.SetFilename(filename)
+    reader.Update()
+    acq = reader.GetOutput()
+    
+    name_emg = ["VL","Gastroc","RF","IJ","TA"]
+    
+    emg_R = {}
+    emg_L = {}
+    
+    for muscle_name in name_emg:
+        emg_R[muscle_name] = acq.GetPoint.GetValues("R"+muscle_name)
+        emg_L[muscle_name] = acq.GetPoint.GetValues("L"+muscle_name)
+
+ # Initialisation des lists contenant les evenements
+    FO = []
+    FO_CL = []
+    FS = []
+    FS_CL = []
+    for it in btk.Iterate(acq.GetEvents()): 
+        if it.GetContext().lower() == side:
+            if it.GetLabel() == 'Foot Strike':
+                FS.append(it.GetFrame())
+            elif it.GetLabel() == 'Foot Off':
+                FO.append(it.GetFrame())
+        elif it.GetContext().lower() == side_cl:
+            if it.GetLabel() == 'Foot Strike':
+                FS_CL.append(it.GetFrame())
+            elif it.GetLabel() == 'Foot Off':
+                FO_CL.append(it.GetFrame())
+                
+numbre_emg = len(name_emg)
+
+fig,axis = plt.subplots(numbre_emg*2,1,figsize=(8.27,11.69),dpi=100)
+
+
+for ind_muscle, muscle_name in enumerate(name_emg):
+    ax_temp_R = axis[ind_muscle*2,1]
+    ax_temp_L =  axis[ind_muscle*2+1,1]
+    
+    ax_temp_R.plot(emg_R[muscle_name])
+    ax_temp_L.plot(emg_L[muscle_name])
