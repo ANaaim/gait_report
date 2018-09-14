ea91f1089a2ae862788fb4eac275edc6716f637f
Alexandre Naaim <alexandre.naaim@gmail.com>
2018-09-14 17:33:34 +0200

    INtegration du calcul avec la minimisation de la variance du knee AA

---
+++ calculation_kindyn.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+import logging
+from argparse import Namespace
+import os
+from tkFileDialog import askopenfilenames, askopenfilename
+from extraction_enf import extraction_enf as extraction_enf
+from extract_metaData_pycgm2 import extract_metaData_pycgm2 as extract_metaData_pycgm2
+from extraction_name_enf import extraction_name_enf as extraction_name_enf
+# pyCGM2 libraries
+import pyCGM2
+# from pyCGM2 import enums
+from pyCGM2.Model.CGM2.coreApps import cgm2_1, cgmUtils
+from pyCGM2.Model import modelDecorator
+from pyCGM2.Tools import btkTools
+from pyCGM2.Utils import files
+# from pyCGM2.Eclipse import vskTools
+
+from shutil import copy2
+
+
+# Choix des fichiers à traiter
+# report_directory = r'C:\Users\VICON\Desktop\Faux Patient'
+
+# filenames_stat = askopenfilename(title="Choisir le fichiers de statique :",
+#                                  filetypes=[("Fichiers C3D", "*Cal*.c3d")],
+#                                  initialdir=report_directory)
+
+# filenames_dyn = askopenfilenames(title="Choisir les fichiers de la première condition:",
+#                                  filetypes=[("Fichiers C3D", "*.c3d")],
+#                                  initialdir=os.path.split(filenames_stat)[0])
+
+
+def calculation_kindyn(filenames_stat, filenames_dyn):
+
+    # Definition des différents répertoires
+    DATA_PATH = os.path.split(filenames_stat)[0] + '\\'
+    DATA_PATH_OUT = os.path.join(DATA_PATH, 'Post_CGM2_1')
+    if not os.path.isdir(DATA_PATH_OUT):
+        os.mkdir(DATA_PATH_OUT)
+    DATA_PATH_OUT = os.path.join(DATA_PATH, 'Post_CGM2_1') + '\\'
+    calibrateFilenameLabelled = str(os.path.split(filenames_stat)[1])
+
+    # Setting pour la toolbox pycgm2
+    args = Namespace()
+    args.leftFlatFoot = None
+    args.rightFlatFoot = None
+    args.markerDiameter = None
+    args.pointSuffix = None
+    args.fileSuffix = None
+    args.mfpa = None
+    args.proj = None
+    args.check = True
+
+    # --------------------------GLOBAL SETTINGS ------------------------------------
+    # global setting ( in user/AppData)
+    settings = files.openJson(pyCGM2.CONFIG.PYCGM2_APPDATA_PATH, "CGM2_1-pyCGM2.settings")
+
+    fileSuffix = None
+    # settings overloading
+    argsManager = cgmUtils.argsManager_cgm(settings, args)
+    leftFlatFoot = argsManager.getLeftFlatFoot()
+    rightFlatFoot = argsManager.getRightFlatFoot()
+    markerDiameter = argsManager.getMarkerDiameter()
+    pointSuffix = argsManager.getPointSuffix("cgm2_1")
+    momentProjection = argsManager.getMomentProjection()
+    mfpa = argsManager.getManualForcePlateAssign()
+
+    # Translator
+    translators = files.getTranslators(DATA_PATH, "CGM2_1.translators")
+    if not translators:
+        translators = settings["Translators"]
+
+    # Choix de la methode de Hanche
+    hjcMethod = settings["Calibration"]["HJC"]
+
+    # Extraction des meta data
+    required_mp, optional_mp = extract_metaData_pycgm2(filenames_stat)
+
+    # Statique
+    model, acqStatic = cgm2_1.calibrate(DATA_PATH, calibrateFilenameLabelled,
+                                        translators, required_mp, optional_mp,
+                                        leftFlatFoot, rightFlatFoot,
+                                        markerDiameter, hjcMethod,
+                                        pointSuffix)
+
+    for ind_file, filename in enumerate(filenames_dyn):
+        plateforme_mpa = ''
+        name_enf = extraction_name_enf(filename)
+        # on fait une copy du fichier ENF pour pouvoir utiliser la génération de rapport
+        copy2(name_enf, DATA_PATH_OUT)
+        [FP1, FP2] = extraction_enf(filename)
+
+        if FP1 == 'right':
+            plateforme_mpa += 'R'
+        elif FP1 == 'left':
+            plateforme_mpa += 'L'
+        else:
+            plateforme_mpa += 'X'
+
+        if FP2 == 'right':
+            plateforme_mpa += 'R'
+        elif FP2 == 'left':
+            plateforme_mpa += 'L'
+        else:
+            plateforme_mpa += 'X'
+
+        reconstructFilenameLabelled = os.path.split(filename)[1]
+        # Fitting
+        # Pour le premier fichier on 'décore' le modèle pour pouvoir faire
+        # la méthode de dynakad (il faut que le modele ait été utilisé en dynamique)
+        if ind_file == 1:
+            acqGait = cgm2_1.fitting(model, DATA_PATH, reconstructFilenameLabelled,
+                                     translators,
+                                     markerDiameter,
+                                     pointSuffix,
+                                     plateforme_mpa, momentProjection)
+            modelDecorator.KneeCalibrationDecorator(model).calibrate2dof('Left')
+            modelDecorator.KneeCalibrationDecorator(model).calibrate2dof('Right')
+        acqGait = cgm2_1.fitting(model, DATA_PATH, reconstructFilenameLabelled,
+                                 translators,
+                                 markerDiameter,
+                                 pointSuffix,
+                                 plateforme_mpa, momentProjection)
+        # writer
+        if fileSuffix is not None:
+            c3dFilename = str(reconstructFilenameLabelled[:-4] +
+                              "-modelled-" + fileSuffix + ".c3d")
+        else:
+            c3dFilename = str(reconstructFilenameLabelled)
+
+        btkTools.smartWriter(acqGait, str(DATA_PATH_OUT + c3dFilename))
+        logging.info("c3d file (%s) generated" % (c3dFilename))
+    return DATA_PATH_OUT
--- rapport_massue.py
+++ rapport_massue.py
@@ -10,7 +10,7 @@ from param_spt_allfiles import param_spt_allfiles as param_spt_allfiles
 from kinematic_allfiles import kinematic_allfiles as kinematic_allfiles
 from kinetic_allfiles import kinetic_allfiles as kinetic_allfiles
 # pour demander à l'utilisateur de sélectionner des fichiers
-from tkFileDialog import askopenfilenames
+from tkFileDialog import askopenfilenames, askopenfilename
 from tkSimpleDialog import askstring
 import tkMessageBox
 from plot_kinematic import plot_kinematic as plot_kinematic
@@ -20,6 +20,7 @@ from extract_Schwartz_norm import extract_Schwartz_norm as extract_Schwartz_norm
 from plot_emg import plot_emg as plot_emg
 import os
 import btk
+from calculation_kindyn import calculation_kindyn as calculation_kindyn
 
 # Demande à l'utilisateur des choses qu'ils souhaitent faire
 comparaison_bool = tkMessageBox.askyesno("Title",
@@ -44,13 +45,30 @@ filenames_case1 = askopenfilenames(title="Choisir les fichiers de la première c
 one_filename = filenames_case1[0]
 subject_directory_ind_1 = [i for i in range(len(one_filename)) if one_filename.startswith('/', i)]
 subject_directory_1 = one_filename[0:subject_directory_ind_1[-1]]
-#subject_name = str(one_filename[subject_directory_ind_1[-3] + 1:subject_directory_ind_1[-2]])
+subject_directory_initial = one_filename[0:subject_directory_ind_1[-1]]
+# choix du fichier statique pour le cas 1
+filenames_stat_case1 = askopenfilename(title="Choisir le fichiers de statique cas 1:",
+                                       filetypes=[("Fichiers C3D", "*Cal*.c3d")],
+                                       initialdir=subject_directory_1)
 
+
+# subject_name = str(one_filename[subject_directory_ind_1[-3] + 1:subject_directory_ind_1[-2]])
+
+# calculs de la cinématique avec la tool box et changement du repertoire
+# contenant les fichiers
+test = calculation_kindyn(filenames_stat_case1, filenames_case1)
+# On change les fichiers
+t = ()
+for name_file in filenames_case1:
+    temp = os.path.join(os.path.split(one_filename)[0], 'Post_CGM2_1', os.path.split(name_file)[1])
+    t = t + (temp,)
+filenames_case1 = t
+subject_directory_1 = os.path.split(filenames_case1[0])[0]
 # Definition de l'extension pgm en fonction du repertoire dans lequel il est
 last_repertory = os.path.split(subject_directory_1)[1]
 if 'CGM' in last_repertory:
     posCGM = last_repertory.find('CGM')
-    extension_pycgm2_case1 = str('_cgm'+last_repertory[posCGM+3:])
+    extension_pycgm2_case1 = str('_cgm' + last_repertory[posCGM + 3:])
 else:
     extension_pycgm2_case1 = ''
 
@@ -88,16 +106,30 @@ else:
 # si l'utilisateurs veut comparer à autre chose on choisit d'autre fichier
 if comparaison_bool:
     filenames_case2 = askopenfilenames(title="Choisir les fichiers de la deuxième condition:", filetypes=[("Fichiers C3D", "*.c3d")],
-                                       initialdir=subject_directory_1)
+                                       initialdir=subject_directory_initial)
     one_filename = filenames_case2[0]
 
     subject_directory_ind_2 = [i for i in range(
         len(one_filename)) if one_filename.startswith('/', i)]
-    subjectory_directory_2 = one_filename[0:subject_directory_ind_1[-1]]
-    last_repertory = os.path.split(subjectory_directory_2)[1]
+    subject_directory_2 = one_filename[0:subject_directory_ind_1[-1]]
+    # choix du fichier statique pour le cas 1
+    filenames_stat_case2 = askopenfilename(title="Choisir le fichiers de statique cas 1:",
+                                           filetypes=[("Fichiers C3D", "*Cal*.c3d")],
+                                           initialdir=subject_directory_initial)
+    # contenant les fichiers
+    test = calculation_kindyn(filenames_stat_case2, filenames_case2)
+    # On change les fichiers
+    t = ()
+    for name_file in filenames_case2:
+        temp = os.path.join(os.path.split(one_filename)[
+                            0], 'Post_CGM2_1', os.path.split(name_file)[1])
+        t = t + (temp,)
+    filenames_case2 = t
+    subject_directory_2 = os.path.split(filenames_case2[0])[0]
+    last_repertory = os.path.split(subject_directory_2)[1]
     if 'CGM' in last_repertory:
         posCGM = last_repertory.find('CGM')
-        extension_pycgm2_case2 = str('_cgm'+last_repertory[posCGM+3:])
+        extension_pycgm2_case2 = str('_cgm' + last_repertory[posCGM + 3:])
     else:
         extension_pycgm2_case2 = ''
 
@@ -292,3 +324,4 @@ if comparaison_bool:
                      legend_1="Droite " + case1_name,
                      legend_2="Droite " + case2_name,
                      title="Kinetic_Comparaison_Right")
+print(report_directory)
